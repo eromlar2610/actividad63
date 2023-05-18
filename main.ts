b2e738f4d5d44653e8c611b04114c79d14cff573
eromlar2610 <132548060+eromlar2610@users.noreply.github.com>
2023-05-18 08:50:46 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let b = 0
+basic.forever(function () {
+    b = 225 - input.lightLevel()
+    led.setBrightness(255)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
